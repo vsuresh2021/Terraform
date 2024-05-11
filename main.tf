85d2b3e2da43f40a7d2028abfaa84557f4722169
vsuresh2021 <83204145+vsuresh2021@users.noreply.github.com>
2024-05-11 10:27:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,14 +16,14 @@ provider "azurerm" {
 
 resource "azurerm_network_security_group" "example" {
   name                = "example-security-group"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = "Central India (Zone 1)"
+  resource_group_name = "Suresh-RG"
 }
 
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = "Central India (Zone 1)"
+  resource_group_name = "Suresh-RG"
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
