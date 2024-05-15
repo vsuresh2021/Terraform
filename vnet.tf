163c78fc6dcf480c4f57d7bc7b3ebca2257b1fd5
vsuresh2021 <83204145+vsuresh2021@users.noreply.github.com>
2024-05-15 09:48:30 +0530

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -43,11 +43,5 @@ resource "azurerm_subnet" "example4" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_subnet" "example4" {
-  name                 = "my-subnet4"
-  resource_group_name  = "Suresh-RG"
-  virtual_network_name = azurerm_virtual_network.example1.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
 
 
